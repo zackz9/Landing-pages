5573fefa404eb31041e0c36a37911d50caefce0b
zackz9 <zackel7593@gmail.com>
2020-12-22 15:00:47 +0000

    Commit 3

---
+++ Landing Page 4/main.js
@@ -0,0 +1,8 @@
+
+  function toggleMenu() {
+    const sect = document.querySelector('#sec');
+    const navig = document.querySelector('#navig');
+    sect.classList.toggle('active');
+    navig.classList.toggle('active');
+   
+  } 
